ef29eeb8e53d50908dff1879eaf7b9b80e801829
Luisa Burini <luisa.burini@gmail.com>
2024-05-28 18:58:49 -0300

    :construction: Equalizando repositorio com alteracoes do cenario

---
+++ android/build/build/intermediates/assets/debug/mergeDebugAssets/Inventory.gd
@@ -0,0 +1,22 @@
+extends Node
+
+enum ITEM_TYPE {vinyl, livro_magico, pagina_morro, pagina_praca}
+
+var items_list = []
+
+func add_item(item):
+	items_list.append(item)
+		
+func check_if_item_exists(item):
+	for it in items_list:
+		if it == item:
+			return true
+	return false
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
